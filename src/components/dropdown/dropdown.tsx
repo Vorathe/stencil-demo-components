e45def83697342eab7a6e05432454702d2a2b21c
Daniel Barbour <daniel.devnull@gmail.com>
2018-07-05 22:18:34 -0500

    Update transform

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -27,7 +27,7 @@ export class Dropdown {
   animateList(isOpen: boolean): void {
     const slideAnimation = [
       {
-        transform: 'translate3d(0, -20%, 0)',
+        transform: 'translate3d(0, -16%, 0)',
         opacity: '0',
         offset: 0
       },
